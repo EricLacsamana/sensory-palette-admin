61bfa68df11d6169dd655cc2d4142256cc1d51b6
Eric Lacsamana <eric@Erics-MacBook-Pro.local>
2026-03-20 15:15:50 +0800

    remoce static env

--- next.config.ts
+++ next.config.ts
@@ -26,9 +26,9 @@ const nextConfig: NextConfig = {
     /* config options here */
 
     // 2. Inject the dynamic URL into your Next.js app
-    env: {
-        NEXT_PUBLIC_API_URL: strapiUrl,
-    },
+    // env: {
+    //     NEXT_PUBLIC_API_URL: strapiUrl,
+    // },
 
     // 3. Whitelist the dynamic IP so Next.js doesn't block Strapi images
     images: {
